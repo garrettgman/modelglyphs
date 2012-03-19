08348a9a1b4931c0328b8390003275d55f75de03
garrettgman <grolemund@gmail.com>
2012-03-19 08:54:09 -0500

    changed argument names in update to avoid default recursion error

--- R/update.r
+++ R/update.r
@@ -1,5 +1,6 @@
 #' @S3method update mg_ensemble
-update.mg_ensemble <- function(object, data_set = data_set(object), groups = groups(object), x_major = x_major(object), y_major = y_major(object), model_info = model_info(object),...) {
+update.mg_ensemble <- function(object, data_set. = data_set(object), groups. = groups(object), x_major. = x_major(object), y_major. = y_major(object), model_info. = model_info(object),...) {
+	# is there a better way to collect the arguments?
 	
 	if (length(match.call()) > 2) {
 		data_set(object) <- data_set
@@ -21,11 +22,11 @@ update.mg_ensemble <- function(object, data_set = data_set(object), groups = gro
 	# 5. Return complete ensemble
 	# faster to just save over object?	
 	structure(models,
-		data_set = data_set,
-		groups = groups, 
-		x_major = x_major, 
-		y_major = y_major, 
-		model_info = model,
+		data_set = data_set(object),
+		groups = groups(object), 
+		x_major = x_major(object), 
+		y_major = y_major(object), 
+		model_info = model(object),
 		key = key,
 		collate = collate,
 		class = c("mg_ensemble", "list"))
