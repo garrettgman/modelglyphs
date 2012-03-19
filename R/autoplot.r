255561e2d0f5da4015c399b64719f46c03d54cb1
Garrett <grolemund@gmail.com>
2012-03-19 16:37:39 -0500

    Wrote color coded autoplot.mg_coef method

--- R/autoplot.r
+++ R/autoplot.r
@@ -9,10 +9,23 @@ autoplot.glyphplot <- function(object, ...) {
 #' @S3method autoplot mg_coef
 autoplot.mg_coef <- function(object, variable = NULL, ...) {
 	require(ggplot2)
-	variable <- as.character(substitute(variable))
-	if (length(variable) == 0) stop("missing argument: variable")
+	if (is.null(variable)) stop("missing argument: variable")
 	object <- object[object$variable == variable,]
-	qplot(x, y, data = object, geom = "tile", group = gid, fill = coefficient)
+	
+	max.value <- max(abs(range(object$coefficient))) + 1e-3
+	values <- c(-max.value, 0, max.value)
+	
+	ggplot(object, aes(x,y)) + 
+		geom_point(aes(color = coefficient, size = abs(coefficient))) + 
+		scale_colour_gradientn(colours = RColorBrewer::brewer.pal(11, 
+			"RdYlBu"), values = values, rescaler = function(x, ...) x, 
+			oob = identity) +
+		scale_area()
+
+	
+	
+	# qplot(x, y, data = object, geom = "tile", group = gid, 
+	#	fill = coefficient)
 	# qplot(x, y, data = object, geom = "point", size = coefficient)
 }
 
@@ -27,19 +40,3 @@ autoplot.mg_ensemble <- function(object, ...) {
 	object <- add_labels(ldply(object, get_r2), object)
 	qplot(x, y, data = object, geom = "point", size = V1)
 }
-
-fill_plot <- function(x, y, data, aes.var) {
-	ggplot(data, aes(x,y, size = substitute(aes.var))) + geom_point()
-}
-	ggplot(data, aes(x, y, fill = var)) + geom_tile()
-}
-c3 <- subset(c2, variable == "temperature")
-ggplot(c3, aes(x,y)) + geom_point(aes(size = coefficient, color = coefficient, fill = coefficient), shape = 15) + scale_area(limits = range(c2$coefficient))
-
-ggplot(c2, aes(x,y)) + geom_point(aes(color = coefficient, size = abs(coefficient)))
-
-q <- ggplot(c3, aes(x,y))
-
-values <- c(-max(abs(range(c3$coefficient))), 0, max(abs(range(c3$coefficient))))
-q + geom_point(aes(color = coefficient), size = 7) + scale_colour_gradientn(colours = rainbow(2),
-  values = values, rescaler = function(x, ...) x, oob = identity)
