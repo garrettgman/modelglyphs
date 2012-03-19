51024f7544a33cacf444ebfb6ffbc43f2341b9fd
garrettgman <grolemund@gmail.com>
2012-03-19 15:53:14 -0500

    exploring autoplot options for coef()

--- R/autoplot.r
+++ R/autoplot.r
@@ -27,3 +27,19 @@ autoplot.mg_ensemble <- function(object, ...) {
 	object <- add_labels(ldply(object, get_r2), object)
 	qplot(x, y, data = object, geom = "point", size = V1)
 }
+
+fill_plot <- function(x, y, data, aes.var) {
+	ggplot(data, aes(x,y, size = substitute(aes.var))) + geom_point()
+}
+	ggplot(data, aes(x, y, fill = var)) + geom_tile()
+}
+c3 <- subset(c2, variable == "temperature")
+ggplot(c3, aes(x,y)) + geom_point(aes(size = coefficient, color = coefficient, fill = coefficient), shape = 15) + scale_area(limits = range(c2$coefficient))
+
+ggplot(c2, aes(x,y)) + geom_point(aes(color = coefficient, size = abs(coefficient)))
+
+q <- ggplot(c3, aes(x,y))
+
+values <- c(-max(abs(range(c3$coefficient))), 0, max(abs(range(c3$coefficient))))
+q + geom_point(aes(color = coefficient), size = 7) + scale_colour_gradientn(colours = rainbow(2),
+  values = values, rescaler = function(x, ...) x, oob = identity)
