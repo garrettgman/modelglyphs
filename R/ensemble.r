780a05f6871927cebdc6835304d6dcae1193fb79
garrettgman <grolemund@gmail.com>
2012-03-20 09:47:07 -0500

    Moved mg_ensemble related functions into ensemble.r

--- R/ensemble.r
+++ R/ensemble.r
@@ -32,3 +32,27 @@ ensemble <- function(data, groups, model, x_major = NULL, y_major = NULL) {
 		class = c("mg_ensemble", "list"))
 	update(half.empty)
 }
+
+
+#' @S3method format mg_ensemble
+format.mg_ensemble <- function(x, ...) {
+	cat(paste("ensemble of", length(x), "models:"), 
+		paste("\nmethod =", model_info(x)$FUN, "\nformula =", 
+		deparse(model_info(x)$formula)))
+}
+
+#' @S3method print mg_ensemble
+print.mg_ensemble <- function(x, ...) {
+	format(x)
+}
+
+#' Is x an ensemble object?
+#'
+#' is.ensemble tests whether an object inherits from the mg_ensemble class.
+#'
+#' @param x An object to be tested for membership in the S3 mg_ensemble class
+#'
+#' @export
+is.ensemble <- function(x) {
+	inherits(x, "mg_ensemble")
+}
