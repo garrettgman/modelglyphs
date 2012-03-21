a0ef18efc121be0ec494f29b0051907ed1fa504b
garrettgman <grolemund@gmail.com>
2012-03-21 12:30:54 -0500

    rewrote coef functions to use mg

--- R/coef.r
+++ R/coef.r
@@ -2,9 +2,7 @@
 coef.mg_ensemble <- function(object, ...){
 	coefs <- ldply(object, coef)
 	coefs <- reshape2::melt(coefs, id = "gid", value.name = "coefficient")
-	output <- add_labels(coefs, object)
-	attr(output, "model_info") <- model_info(object)
-	add_class(output, "mg_coef")
+	mg(coefs, object, "mg_coef")
 }
 
 #' S3method coef mg_summary
@@ -14,9 +12,7 @@ coef.mg_summary <- function(object, ...){
 		cbind(variable = row.names(cfs), as.data.frame(cfs))
 	}
 	output <- ldply(object, coef_df, ...)
-	output <- add_labels(output, object)
-	attr(output, "model_info") <- model_info(object)
-	add_class(output, "mg_coef_summary")
+	mg(output, object, "mg_coef_summary")
 }
 
 
@@ -26,16 +22,10 @@ autoplot.mg_coef <- function(object, variable = NULL, ...) {
 	if (is.null(variable)) stop("missing argument: variable")
 	stopifnot(variable %in% object$variable)
 	object <- object[object$variable == variable,]
-	
-	max.value <- max(abs(range(object$coefficient))) + 1e-3
-	values <- c(-max.value, 0, max.value)
-	
-	ggplot(object, aes(x,y)) + 
-		geom_point(aes(color = coefficient, size = abs(coefficient)), ...) + 
-		scale_colour_gradientn(colours = RColorBrewer::brewer.pal(11, 
-			"RdYlBu")[11:1], values = values, rescaler = function(x, ...) x, 
-			oob = identity) +
-		scale_area()
+
+	plot.title <- paste("Magnitudes of", variable, "\n", mg_call(object))
+
+	magnitude_plot(object, "coefficient", title = plot.title, ...)	
 }
 
 
@@ -49,8 +39,7 @@ autoplot.mg_coef_summary <- function(object, variable = NULL, p.value = "Pr(>|t|
 	color.var <- ifelse(color, "Estimate", NULL)
 	
 	info <- model_info(object)
-	plot.title <- paste(variable, " coefficients for\n", info$FUN, 
-		"(", deparse(info$formula), ")", sep = "")
+	plot.title <- paste("Significance of", variable, "\n", mg_call(object))
 	
 	significance_plot(object, p.value, color.var, 
 		title = plot.title, ...)
