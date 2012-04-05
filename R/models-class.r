571e90e3a907cb4a2ee56783115253cdfd96545b
Garrett <grolemund@gmail.com>
2012-04-05 15:05:55 -0500

    wrote format, print, and is methods for models class

---
+++ R/models-class.r
@@ -0,0 +1,17 @@
+#' Is an object a models object from the modelglyphs package?
+#'
+#' @param x An object to be tested
+#' @return logical TRUE if x inherits from the 'models' class, FALSE otherwise
+#' @export
+is.models <- function(x) {
+	inherits(x, "models")
+}
+
+format.models <- function(x, ...){
+	cat(paste("An ensemble of ", length(x), " models:\n\nCall:\n", 
+		deparse(x[[1]]$call), "\n", sep = ""))
+}
+
+print.models <- function(x, ...){
+	format(x)
+}
